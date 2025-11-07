76371be4afb78722f4ad7b4fefbc3afac7e7863c
olu <oluegbiaye@gmail.com>
2025-11-07 19:43:01 +0100

    Refactor: Disable cache and improve thumbnail upload
    Disable cache to prevent unexpected behavior. Improve thumbnail upload
    by checking file type, file size, and saving the file to disk. Add
    logging.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
 	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -30,47 +35,79 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "failed getting video", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "wrong id was given", nil)
+		return
+	}
+	log.Println("uploading thumbnail for video:", videoID, "by user:", userID)
 
 	// TODO: implement the upload here
+	const maxUploadSize int64 = 10 << 20
+	if err = r.ParseMultipartForm(maxUploadSize); err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse Multipart form", err)
+		return
+	}
 
-	const maxMemory int64 = 10 << 20
-	r.ParseMultipartForm(maxMemory)
-	file, header, err := r.FormFile("thumbnail")
+	file, _, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
 		return
 	}
 	defer file.Close()
 
-	imageData, err := io.ReadAll(file)
+	fileBytes, err := io.ReadAll(file)
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "failed reading form file", err)
 		return
 	}
 
-	video, err := cfg.db.GetVideo(videoID)
-	if err != nil {
-		respondWithError(w, http.StatusNotFound, "failed getting video", err)
+	if int64(len(fileBytes)) > maxUploadSize {
+		respondWithError(w, http.StatusBadRequest, "File size exceeds the limit of 10MB", nil)
 		return
 	}
-	if video.UserID != userID {
-		respondWithError(w, http.StatusUnauthorized, "wrong id was given", nil)
-		return
-	}
-	var (
-		encodedImgData string = base64.StdEncoding.EncodeToString(imageData)
-		mediaType      string = header.Header.Get("Content-Type")
-	)
-	videoThumbnails[video.ID] = thumbnail{data: imageData, mediaType: mediaType}
 
-	thumbnailURL := "data:" + mediaType + ";base64," + encodedImgData
+	contentType := http.DetectContentType(fileBytes)
+	allowedTypes := map[string]bool{
+		"image/jpeg": true,
+		"image/png":  true,
+		"image/gif":  true,
+	}
+	if !allowedTypes[contentType] {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Unsupported file type: %s", contentType), nil)
+		return
+	}
+
+	fileExt, err := mime.ExtensionsByType(contentType)
+	if err != nil || len(fileExt) == 0 {
+		respondWithError(w, http.StatusInternalServerError, "Could not determine file extension", err)
+		return
+	}
+
+	key := make([]byte, 32)
+	rand.Read(key)
+	URLEncoding := base64.RawURLEncoding.EncodeToString(key)
+	fileName := URLEncoding + fileExt[0]
+	assetsFilePath := filepath.Join(cfg.assetsRoot, fileName)
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, fileName)
+
+	// Write the validated file data to disk in one step
+	if err := os.WriteFile(assetsFilePath, fileBytes, 0o644); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to save file to disk", err)
+		return
+	}
+
 	video.ThumbnailURL = &thumbnailURL
-
 	if err = cfg.db.UpdateVideo(video); err != nil {
 		respondWithError(w, http.StatusBadRequest, "failed updating video metadata", err)
 		return
 	}
 
+	log.Printf("Successfully saved thumbnail for video %s at %s", video.ID, assetsFilePath)
 	respondWithJSON(w, http.StatusOK, video)
 }
